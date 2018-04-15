a45f139082fbe7febde3860f01b1a1849aa14129
Ishi Ruy <dev@nhz.io>
2018-04-15 17:21:23 +0300

    cleanup

--- pirl-node/start.sh
+++ pirl-node/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pirl --etherbase "0x85fe98a34d4e9c3d1163d34efd07eb35e80b7397" --rpc --rpcaddr 0.0.0.0 --rpccorsdomain * --rpcport 6588  --rpcapi "eth,net,web3"
+pirl --etherbase "$ETHERBASE" --rpc --rpcaddr 0.0.0.0 --rpccorsdomain * --rpcport 6588  --rpcapi "eth,net,web3"
