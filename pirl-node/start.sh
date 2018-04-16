18af564a2d8cbbede9edd93de23095f565adb944
Ishi Ruy <dev@nhz.io>
2018-04-16 12:07:57 +0300

    add dashboard

--- pirl-node/start.sh
+++ pirl-node/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-pirl --etherbase "$ETHERBASE" --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --rpcvhosts "*" --rpcport 6588  --rpcapi "eth,net,web3"
+pirl --etherbase "$ETHERBASE" --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --rpcvhosts "*" --rpcport 6588  --rpcapi "eth,net,web3" --dashboard --dashboard.addr 0.0.0.0 --dashboard.host 8080
